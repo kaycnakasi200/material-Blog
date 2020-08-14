0d76f32dfddfb8c02bad3775aedcad95f1a8874d
besic-web <kaycnakasi200@gmail.com>
2020-08-14 15:44:02 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,112 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import { Container } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+
+const useStyles = makeStyles((theme) => ({
+  AppBar: {
+    backgroundColor: "fff"
+  },
+  hero: {
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url
+    ('https://www.freepik.com/free-vector/fashion-banner-blog_9472301.htm#page=1&query=web%20banner%20blog&position=5')`,
+    height: "500px",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repear",
+    backgroundSize: "cover",
+    position: "relative",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "#fff",
+    fontSize: "4rem",
+  },
+  blogContainer: {
+    paddingTop: theme.spacing(3)
+  },
+  blogTitle: {
+    fontWeight: 800,
+    paddingTop: theme.spacing(3)
+  },
+  card: {
+    maxWidth: 240
+  },
+  CardActionArea:{
+      display: "flex",
+      margin: "0 10px",
+      justifyContent: "space-between"
+  },
+  author:{
+    display:"flex"
+  }
+
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar className={classes.appBar} position="static">
+        <Toolbar>
+          <Typography variant="h6" color="warning" >
+            Blog
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box className={classes.hero}>
+        <Box>React Blog </Box>
+      </Box>
+      <Container maxWidth="lg" className={classes.blogContainer}>
+        <Typography variant="h4" className={classes.blogTitle}>
+          Articles
+          </Typography>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image="https://www.pexels.com/photo/man-in-grey-sweater-holding-yellow-sticky-note-879109/"
+                  title="Contemplative Reptile"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    React useContext
+          </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                    across all continents except Antarctica
+          </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions className={classes.CardActionArea}>
+              <Box className={classes.author}>
+                
+              </Box>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
---
+++ src/Theme.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const  theme = createMuiTheme({
+    palette:{
+        main: "#ee21dc"
+    },
+    backgroud:{
+        default:"#ffffff"
+    }
+})
+
+export default theme;
